cb0b0b7f6c1707586093ef000ea6d466197a0a86
rounak1619 <rounakmehata9@gmail.com>
2021-09-09 06:11:54 +0000

    lemonkebab:Build gapps and nuke livedisplay stuff

--- p404_lemonkebab.mk
+++ p404_lemonkebab.mk
@@ -23,6 +23,8 @@ $(call inherit-product, device/oneplus/lemonkebab/device.mk)
 
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/404/configs/common.mk)
+$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
